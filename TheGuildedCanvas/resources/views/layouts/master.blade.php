671d03f03ac2431ba7979576bfeee628d64abd8a
HafsaMuse433 <220034513@aston.ac.uk>
2025-02-21 22:58:11 +0000

    imported new font to make text look more professional and improved the spacing between the text

--- TheGuildedCanvas/resources/views/layouts/master.blade.php
+++ TheGuildedCanvas/resources/views/layouts/master.blade.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link
+  href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap"
+  rel="stylesheet"
+/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="{{ asset('images/TGC-Black-and-Gold.png') }}">
